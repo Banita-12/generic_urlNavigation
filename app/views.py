258aa467ebd0f58ec8328c1048927ecb64045d82
Banita-12 <banitasamantara023@gmail.com>
2023-11-20 22:29:41 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def situn(request):
+    return render(request,'situn.html')
+
+def sila(request):
+    return render(request,'sila.html')
+
+
+def situn2(request):
+    return render(request,'situn2.html')
